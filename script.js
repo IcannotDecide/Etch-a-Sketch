58eef17bf0a60d6978e2d83c2cfd7cb801193d56
IcannotDecide <matthewat43@gmail.com>
2023-06-27 14:54:36 +0100

    Add Change grid size feature

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-let gridSize = 16;
-
 const mainContainer = document.querySelector(".mainContainer");
 
-for (let i = 0; i < gridSize; i++) {
-    mainContainer.innerHTML += '<div class="container"></div>'
-    for (let j = 0; j < gridSize; j++) {
-        let container = mainContainer.lastChild;
-        container.innerHTML += '<div class="box"></div>'
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        mainContainer.innerHTML += '<div class="container"></div>'
+        for (let j = 0; j < size; j++) {
+            let container = mainContainer.lastChild;
+            container.innerHTML += '<div class="box"></div>'
+        };
     };
 };
 
+createGrid(16);
+
+
+function createBoxOnHover() {
 const box = document.querySelectorAll(".box");
 box.forEach(div => div.addEventListener('mouseover', hover), {
     capture: false
-});
+})
+};
 
 function hover(e) {
-    e.target.style.backgroundColor === "" ? 
-    e.target.style.backgroundColor = "pink" :
-    e.target.style.backgroundColor = "";
+    e.target.style.backgroundColor === "" ?
+        e.target.style.backgroundColor = "pink" :
+        e.target.style.backgroundColor = "";
     e.stopPropagation();
 };
+
+function removeContainer() {
+    let container = document.querySelectorAll('.container');
+    for (let i = 0; i < container.length; i++) {
+        container[i].remove()
+    }
+}
+
+function changeGridSize() {
+    const value = prompt("Enter a number that you want the grid size to be")
+
+
+    if (+value && +value < 100) {
+
+        removeContainer()
+        createGrid(+value);
+        createBoxOnHover();
+    } else {
+      alert("please enter a value that is a number and less than 100")
+    }
+}
+
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', changeGridSize);
